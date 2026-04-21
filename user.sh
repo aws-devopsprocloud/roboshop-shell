fe727d4acc2d13ef5ba4657bab79552750c5f4d5
DevOpsProCloud <git-repo@devopsprocloud.in>
2026-04-21 13:05:09 +0530

    user-shell-pc

--- user.sh
+++ user.sh
@@ -27,3 +27,52 @@ then
 else 
     dnf module disable nodejs -y &>> $LOGFILE
     VALIDATE $? "Disabling NodeJS" 
+
+    dnf module enable nodejs:20 -y &>> $LOGFILE
+    VALIDATE $? "Enabling NodeJS:20"
+
+    dnf install nodejs -y &>> $LOGFILE
+    VALIDATE $? "Installing NodeJS:20"
+
+    id roboshop &>> $LOGFILE
+    if [ $? -ne 0 ]
+    then 
+        useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>> $LOGFILE
+        VALIDATE $? "Creating Roboshop system user"
+    else 
+        echo -e "Roboshop user already Exist $Y SKIPPING $N"
+    fi
+    
+    mkdir -p /app &>> $LOGFILE
+    VALIDATE $? "Creating /app directory"
+
+    curl -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user-v3.zip &>> $LOGFILE
+    VALIDATE $? "Downloading user.zip"
+
+    cd /app &>> $LOGFILE
+    VALIDATE $? "Going to /app directory"
+
+    rm -rf /app/* &>> $LOGFILE
+    VALIDATE $? "Removing the existing code"
+
+    unzip /tmp/user.zip &>> $LOGFILE
+    VALIDATE $? "Unzipping user.zip"
+
+    cd /app &>> $LOGFILE
+    VALIDATE $? "Going to /app directory"
+
+    npm install &>> $LOGFILE
+    VALIDATE $? "Installing Dependencies"
+
+    cp /home/ec2-user/roboshop-shell/user.service /etc/systemd/system/user.service &>> $LOGFILE
+    VALIDATE $? "Copying User Service"
+
+    systemctl daemon-reload &>> $LOGFILE
+    VALIDATE $? "Daemon Reloading"
+
+    systemctl enable user &>> $LOGFILE
+    VALIDATE $? "Enabling User"
+
+    systemctl start user &>> $LOGFILE
+    VALIDATE $? "Starting User"
+fi
