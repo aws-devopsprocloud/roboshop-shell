8607d227b7c71fa38afd64f693f785a7c16ed51f
mac-devopsprocloud <premsagar.eri@devopsprocloud.in>
2026-04-21 13:16:39 +0530

    cart-shell

--- cart.sh
+++ cart.sh
@@ -27,3 +27,52 @@ then
 else 
     dnf module disable nodejs -y &>> $LOGFILE
     VALIDATE $? "Disabling NodeJS" 
+
+    dnf module enable nodejs:20 -y &>> $LOGFILE
+    VALIDATE $? "Enabling NodeJS:20"
+
+    dnf install nodejs -y &>> $LOGFILE
+    VALIDATE $? "Installing NodeJS:20"
+
+    id roboshop &>> $LOGFILE
+    if [ $? -ne 0 ]
+    then 
+        useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>> $LOGFILE
+        VALIDATE $? "Creating Roboshop system user"
+    else 
+        echo -e "Roboshop user already Exist $Y SKIPPING $N"
+    fi
+    
+    mkdir -p /app &>> $LOGFILE
+    VALIDATE $? "Creating /app directory"
+
+    curl -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart-v3.zip &>> $LOGFILE
+    VALIDATE $? "Downloading cart.zip"
+
+    cd /app &>> $LOGFILE
+    VALIDATE $? "Going to /app directory"
+
+    rm -rf /app/* &>> $LOGFILE
+    VALIDATE $? "Removing the existing code"
+
+    unzip /tmp/cart.zip &>> $LOGFILE
+    VALIDATE $? "Unzipping cart.zip"
+
+    cd /app &>> $LOGFILE
+    VALIDATE $? "Going to /app directory"
+
+    npm install &>> $LOGFILE
+    VALIDATE $? "Installing Dependencies"
+
+    cp /home/ec2-cart/roboshop-shell/cart.service /etc/systemd/system/cart.service &>> $LOGFILE
+    VALIDATE $? "Copying Cart Service"
+
+    systemctl daemon-reload &>> $LOGFILE
+    VALIDATE $? "Daemon Reloading"
+
+    systemctl enable cart &>> $LOGFILE
+    VALIDATE $? "Enabling Cart"
+
+    systemctl start cart &>> $LOGFILE
+    VALIDATE $? "Starting Cart"
+fi
