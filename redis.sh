edf8684b585b87a43478330c921a4f6b08ca7a0d
DevOpsProCloud <git-repo@devopsprocloud.in>
2026-04-21 12:03:46 +0530

    redis-shell

---
+++ redis.sh
@@ -0,0 +1,29 @@
+#!/bin/bash 
+
+ID=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+DATE=$(date +%F-%H-%M-%S)
+
+LOGFILE=/tmp/$0-$DATE.log
+
+VALIDATE () {
+    if [ $1 -ne 0 ]
+    then 
+        echo -e "ERROR: $2 ...is $R FAILED $N"
+        exit 1
+    else 
+        echo -e "$2....is $G SUCCESS $N"
+    fi
+}
+
+if [ $ID -ne 0 ]
+then 
+    echo -e "$R ERROR: Please run this script with root access $N"
+else 
+    dnf module disable redis -y &>> $LOGFILE
+    VALIDATE $? "Disabling Redis" 
