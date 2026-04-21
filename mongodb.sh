cc18fa3f5a83284099ed409bf45161ecb332f442
mac-devopsprocloud <premsagar.eri@devopsprocloud.in>
2026-04-21 09:28:33 +0530

    mongodb-shell

---
+++ mongodb.sh
@@ -0,0 +1,42 @@
+#!/bin/bash 
+
+ID=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+DATE=$(date +%F-%H-%M-%S)
+
+LOGFILE=/tmp/$0-$DATE.log
+
+VALIDATE () {
+    if [ $1 -ne 0 ]
+    then 
+        echo "ERROR: $2 ...is $R FAILED $N"
+    else 
+        echo "$2....is $G SUCCESS $N"
+}
+
+if [ $ID -ne 0 ]
+then 
+    echo "$R ERROR: Please run thios script with root access $N"
+else 
+    cp /home/ec2-user/roboshop-shell/mongo.repo /etc/yum.repos.d/mongo.repo &>> $LOGFILE
+    VALIDATE $? "Copying Mongo Repo" 
+
+    dnf install mongodb-org -y &>> $LOGFILE
+    VALIDATE $? "Installing Mongodb"
+
+    systemctl enable mongod &>> $LOGFILE
+    VALIDATE $? "Enabling Mongodb"
+
+    systemctl start mongod &>> $LOGFILE
+    VALIDATE $? "Starting Mongodb"
+
+    sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf &>> $LOGFILE
+    VALIDATE $? "Allowing remote connections"
+
+    systemctl restart mongod &>> $LOGFILE
+    VALIDATE $? "Restarting Mongodb"
