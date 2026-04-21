4d7f7190c858b18f8601f1f831c4d78c07cf8101
mac-devopsprocloud <premsagar.eri@devopsprocloud.in>
2026-04-21 10:03:37 +0530

    catalogue-shell

--- catalogue.sh
+++ catalogue.sh
@@ -15,6 +15,7 @@ VALIDATE () {
     if [ $1 -ne 0 ]
     then 
         echo -e "ERROR: $2 ...is $R FAILED $N"
+        exit 1
     else 
         echo -e "$2....is $G SUCCESS $N"
     fi
@@ -33,10 +34,16 @@ else
     dnf install nodejs -y &>> $LOGFILE
     VALIDATE $? "Installing NodeJS:20"
 
-    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>> $LOGFILE
-    VALIDATE $? "Creating Roboshop system user"
-
-    mkdir /app &>> $LOGFILE
+    id roboshop &>> $LOGFILE
+    if [ $? -ne 0 ]
+    then 
+        useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>> $LOGFILE
+        VALIDATE $? "Creating Roboshop system user"
+    else 
+        echo -e "Roboshop user already Exist $Y SKIPPING $N"
+    fi
+    
+    mkdir -p /app &>> $LOGFILE
     VALIDATE $? "Creating /app directory"
 
     curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip &>> $LOGFILE
@@ -72,7 +79,7 @@ else
     dnf install mongodb-mongosh -y &>> $LOGFILE
     VALIDATE $? "Installing Mongodb Client"
 
-    mongosh --host 172.31.37.170 </app/db/master-data.js
+    mongosh --host 172.31.37.170 </app/db/master-data.js &>> $LOGFILE
     VALIDATE $? "Inserting catalogue data into mongodb"
 
 fi
